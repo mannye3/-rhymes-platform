db363d2b7761c3bd33b48c80869a2044cc5393a1
Aboajah Emmanuel <aboajah.emmanuel@fmdqgroup.com>
2025-10-10 13:17:34 +0100

    Assets Modification

--- config/app.php
+++ config/app.php
@@ -56,7 +56,7 @@ return [
 
      'url'             => env('APP_URL', 'https://rhymes-platform-production.up.railway.app/'),
 
-    'asset_url'       => env('ASSET_URL', 'https://rhymes-platform-production.up.railway.app/public'),
+    'asset_url'       => env('ASSET_URL', 'https://rhymes-platform-production.up.railway.app/'),
 
     /*
     |--------------------------------------------------------------------------
