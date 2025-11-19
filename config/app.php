d8329cf3dc8de4addf2c5a63fec60afbd19ac977
Aboajah Emmanuel <aboajah.emmanuel@fmdqgroup.com>
2025-11-19 18:00:34 +0100

    update

--- config/app.php
+++ config/app.php
@@ -52,8 +52,10 @@ return [
     |
     */
 
-     'url' => env('APP_URL', 'http://localhost'),
-     'asset_url' => env('APP_URL', 'http://localhost'),
+     'url' => env('APP_URL'),
+
+    'asset_url' => env('ASSET_URL'),
+
 
   
 
